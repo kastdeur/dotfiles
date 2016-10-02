7a82ebe7f1d70041d2dca66190fe736cf9d3b3f0
Eric Teunis de Boone <ericteunis@deboone.nl>
2016-10-02 23:39:11 +0200

    Added SXC thingies to dotfiles, allowing for *.local files for bash and profile. Still Needs these files for tmux,vim,git,...

--- profile.sh
+++ profile.sh
@@ -4,6 +4,8 @@ DOTFILES="$HOME/.dotfiles"
 # Source main profile
 [ -r "$DOTFILES/profiles/main.sh" ] && source "$DOTFILES/profiles/main.sh"
 
-
 # Source machine specific profile
-[ -r "$DOTFILES/profiles/$HOSTNAME.profile"] && source "$DOTFILES/profiles/$HOSTNAME.profile"
+[ -r "$DOTFILES/profiles/$HOSTNAME.profile" ] && source "$DOTFILES/profiles/$HOSTNAME.profile"
+
+# Always source a local profile
+[ -r "$HOME/.profile.local" ] && source "$HOME/.profile.local"
