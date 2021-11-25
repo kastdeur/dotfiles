b553944b9601e87ac701cb7c328e368c46378441
Eric Teunis de Boone <ericteunis@deboone.nl>
2021-11-25 15:14:59 +0100

    [ssh] Auto chmod ssh/config to 600

--- install.sh
+++ install.sh
@@ -13,3 +13,7 @@ git pull || echo "Pulling from remote failed"
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+
+
+# Make sure ssh/config is only read/writable for owner
+chmod 600 ssh/config
